a14cb61365d477b1c0c94a6ab4a2c74f580efc36
Rebecca Mrowiec <49169512+rebeccamrowiec@users.noreply.github.com>
2019-08-09 15:06:04 +0100

    Fix issue with moving code

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,34 +1,16 @@
 import React from 'react';
 
-const TodoItem = (props) => {  
-  const text = props.text;
-  
-  // removeItem(e) {
-  //   this.props.removeText(item);
-  // }
+class TodoItem extends React.Component {  
+  render () {
+    const { removeText, text } = this.props
 
-  const { removeText } = props
-
-  return (
-    <div className="item">
-      <h3 className="item-text">{text}</h3>
-      <button className="item-button" onClick={() => { removeText(text) }}></button>
-    </div>
-  );
+    return (
+      <div className="item">
+        <h3 className="item-text">{text}</h3>
+        <button className="item-button" onClick={() => { removeText(text) }}></button>
+      </div>
+    );
+  }
 };
 
 export default TodoItem;
-
-
-
-// class TodoItem extends React.Component {
-//   constructor(props) {
-//     super(props);
-//   }
-
-//   render() {
-//     return (
-//       <h3>{props.text}</h3>
-//     );
-//   }
-// }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,21 +4,11 @@ import TodoItem from './TodoItem';
 
 class TodoList extends React.Component {  
   render() {
-
     const { removeText } = this.props
-    console.log(removeText);
-
-    // const texts = this.props.texts.map((text, i) => {
-    //   return (
-    //     <div className="item" key={i}>
-    //       <h3 className="item-text">{text}</h3>
-    //       <button className="item-button" onClick={() => { removeText(text) }}></button>
-    //     </div>
-    //   );
-    // });
+    // console.log(removeText);
 
     const texts = this.props.texts.map((text, i) => {
-      return <TodoItem key={i} text={text} />;
+      return <TodoItem key={i} text={text} removeText={removeText} />;
     });
 
     return <div>{texts}</div>;
